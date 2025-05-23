e3089dd3d586cc224f857193dedbb127ec61ccc2
mjmin99 <mjmin99@naver.com>
2025-05-23 17:38:17 +0900

    [feat] 애니메이션 구현
    이동과 각 상태에 따른 애니메이션을 구현했다.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,29 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; // 인풋 시스템 사용하려면 해야함
+using UnityEngine.InputSystem; // 인
 
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed;
+
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-   
+    private void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
 
     private void FixedUpdate()
     {
-        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
     }
 
-    void OnMove(InputValue value)
+    private void LateUpdate()
     {
-        inputVec = value.Get<Vector2>();
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
