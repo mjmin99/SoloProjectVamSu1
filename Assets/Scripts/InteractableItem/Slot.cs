150e074ab959ba500f73b45ce682bc4518502776
mjmin99 <mjmin99@naver.com>
2025-05-30 17:20:47 +0900

    [feat] 인벤토리 구현
    드래그앤드롭을 통해 아이템을 인벤토리 내에서 옮길 수 있다.

--- Assets/Scripts/InteractableItem/Slot.cs
+++ Assets/Scripts/InteractableItem/Slot.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Unity.VisualScripting;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour , IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
+    private Vector3 originPos;
+
     public Items items;
     public int itemCount;
     public Image itemImage;
 
+
+    void Start()
+    { 
+        originPos = transform.position;
+    }
     public void Additem(Items _items)
     {
         items = _items;
@@ -31,5 +40,71 @@ public class Slot : MonoBehaviour
         itemCount = 0;
         SetColor(0);
 
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            if (items != null)
+            {
+                if (items.itemType == Items.ItemType.Equipment)
+                {
+                    Debug.Log(items.itemName + "을 장착했습니다.");
+                }
+                else
+                {
+                    Debug.Log(items.itemName + "을 사용했습니다.");
+                    ClearSlot();
+                }
+            }
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (items != null)
+        {
+            DragSlot.instance.dragSlot = this;
+            DragSlot.instance.DranSetImage(itemImage);
+            DragSlot.instance.transform.position = eventData.position;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (items != null)
+        {
+            DragSlot.instance.transform.position = eventData.position;
+        }
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DragSlot.instance.SetColor(0);
+        DragSlot.instance.dragSlot = null;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (DragSlot.instance.dragSlot != null)
+        ChangeSlot();
+    }
+
+    private void ChangeSlot()
+    {
+        Items _tempItem = items;
+
+        Additem(DragSlot.instance.dragSlot.items);
+
+        if (_tempItem != null)
+        {
+            DragSlot.instance.dragSlot.Additem(_tempItem);
+        }
+        else
+        { 
+            DragSlot.instance.dragSlot.ClearSlot();
+        }
     }
 }
